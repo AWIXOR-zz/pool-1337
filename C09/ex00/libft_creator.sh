a306109b9003f614cdff74c7b1b4e941d83ca787
El houcine Aouassar <eaouassa@e1r11p3.1337.ma>
2021-10-11 12:50:53 +0100

    C09

--- C09/ex00/libft_creator.sh
+++ C09/ex00/libft_creator.sh
@@ -5,12 +5,11 @@
 #                                                     +:+ +:+         +:+      #
 #    By: eaouassa <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/10/10 12:59:11 by eaouassa          #+#    #+#              #
-#    Updated: 2021/10/10 13:17:20 by eaouassa         ###   ########.fr        #
+#    Created: 2021/10/10 14:13:17 by eaouassa          #+#    #+#              #
+#    Updated: 2021/10/10 14:26:39 by eaouassa         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 gcc -c -Wall -Werror -Wextra ft_putstr.c ft_putchar.c ft_strlen.c ft_strcmp.c ft_swap.c
-ar -rc libft.a ft_putstr.c ft_putchar.c ft_strlen.c ft_strcmp.c ft_swap.c 
-ranlib libft.a
+ar rc libft.a ft_putstr.o ft_putchar.o ft_strlen.o ft_strcmp.o ft_swap.o 
 rm -f *.o
